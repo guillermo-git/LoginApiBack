240026abc10db570233b5ec81764604fa2520d7c
guill <guill@AVALOS>
2024-11-13 19:45:55 -0600

    inicio

---
+++ LoginAPIback/Controllers/usuariosController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SYSMEwebAPIback.Custom;
+using SYSMEwebAPIback.Models.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using SYSMEwebAPIback.Models;
+using System.Linq.Expressions;
+
+namespace SYSMEwebAPIback.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class usuariosController : ControllerBase
+    {
+        private readonly  ScmerbdbContext scmerbdbContext;
+        public usuariosController()
+        {
+            scmerbdbContext = new ScmerbdbContext();
+        }
+
+
+        [HttpGet]
+        [Route("lista")]
+        public async Task<IActionResult> Lista()
+        {
+            var lista =await scmerbdbContext.Usuarios.ToListAsync();
+            return StatusCode(StatusCodes.Status200OK, new { value = lista });
+        }
+
+    }
+}
